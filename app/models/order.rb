80f5e24d8f7c1c2a158940e1adc75c5c27d5aabe
Ananth Amar <ananth.amar@sleekr.co>
2019-02-25 23:02:28 +0700

    Touvch User Lead

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,9 +5,15 @@ class Order < ApplicationRecord
   validates_presence_of :user_lead_id, :vendor_id, :product_id
   has_paper_trail
 
+  before_save :touch_user_lead
+
   def as_object
     as_json(only: %i[id product_id account_number taxed status working_order company_name price installation expiry_date details
 					 created_at])
+  end
+
+  def touch_user_lead
+    user_lead.touch
   end
 
 end
