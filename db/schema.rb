824a6e046610187285c9728615004036b17704e4
Ananth Amar <ananth.amar@sleekr.co>
2019-02-25 18:44:17 +0700

    Gem to PG

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20190224171009) do
 
-  create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
     t.bigint "vendor_id"
     t.integer "product_id"
     t.string "account_number"
     t.string "working_order"
     t.string "company_name"
-    t.float "price", limit: 24
+    t.float "price"
     t.datetime "installation"
     t.datetime "expiry_date"
     t.string "details"
@@ -31,12 +34,12 @@ ActiveRecord::Schema.define(version: 20190224171009) do
     t.index ["vendor_id"], name: "index_orders_on_vendor_id"
   end
 
-  create_table "user_addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "user_addresses", force: :cascade do |t|
     t.bigint "user_id"
     t.string "formatted_address"
     t.string "postal_code"
-    t.float "latitude", limit: 24
-    t.float "longitude", limit: 24
+    t.float "latitude"
+    t.float "longitude"
     t.string "city"
     t.string "country_name"
     t.string "region_name"
@@ -45,7 +48,7 @@ ActiveRecord::Schema.define(version: 20190224171009) do
     t.index ["user_id"], name: "index_user_addresses_on_user_id"
   end
 
-  create_table "user_leads", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "user_leads", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "vendor_id"
     t.datetime "created_at", null: false
@@ -57,7 +60,7 @@ ActiveRecord::Schema.define(version: 20190224171009) do
     t.index ["vendor_id"], name: "index_user_leads_on_vendor_id"
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
@@ -72,18 +75,19 @@ ActiveRecord::Schema.define(version: 20190224171009) do
     t.index ["referral_code"], name: "index_users_on_referral_code"
   end
 
-  create_table "versions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "versions", force: :cascade do |t|
     t.string "item_type", limit: 191, null: false
     t.integer "item_id", null: false
     t.string "event", null: false
     t.string "whodunnit"
-    t.text "object", limit: 4294967295
+    t.text "object"
     t.datetime "created_at"
-    t.text "object_changes", limit: 4294967295
+    t.text "object_changes"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "orders", "user_leads"
+  add_foreign_key "orders", "users", column: "vendor_id"
   add_foreign_key "user_addresses", "users"
   add_foreign_key "user_leads", "users"
   add_foreign_key "user_leads", "users", column: "vendor_id"
