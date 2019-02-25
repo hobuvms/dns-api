df6278e79aa64be49bed0103cd1d10d82df3fac1
Ananth Amar <ananth.amar@sleekr.co>
2019-02-25 18:47:33 +0700

    Gem to PG

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'Hobu Admin', email: 'admin@hobu.com', password: 'hobuadmin', role: :vendor, medium: :direct,
+User.create(name: 'Hobu Admin', email: 'admin@hobu.com', password: 'hobuadmin', role: :vendor,
             referral_code: 'hobuadmin')
